8345ca6e24968dc5afe559bba66b7e1a5f56b213
A.Leschenko <Anton.Leshchenko@infotecs.ru>
2014-12-15 16:23:56 +0300

    Correct declarations

--- pstring.h
+++ pstring.h
@@ -10,7 +10,7 @@ Pstring* pstrcpy(Pstring* dst, Pstring* src);
 
 Pstring* pstrijcpy(Pstring* dst, Pstring* src, char i, char j);
 
-int pstrcmp(Pstring* pstr1, Pstring* psrt2);
+int pstrcmp(Pstring* pstr1, Pstring* pstr2);
 
-int pstrijcmp(Pstring* pstr1, Pstring* psrt2, char i, char j);
+int pstrijcmp(Pstring* pstr1, Pstring* pstr2, char i, char j);
 
